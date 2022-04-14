22b1502a94251a56527a5caf102ec3be68e30e01
StevenCaddy <steven.caddie@gmail.com>
2022-04-14 18:03:17 -0500

    writing to doc users
    for loop not quite working

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+$(document).ready(function(){
+    
+    const request = () => {
+        fetch(`https://jsonplaceholder.typicode.com/users/`)
+        .then((res) => res.json())
+        .then((res) => {
+            const user = document.getElementById('users')
+            console.log(res)
+            res.forEach(element => {
+                user.innerHTML = `${element.name}`
+            });
+            
+        })
+        .catch((err) => console.error(err));
+    }
+    
+    
+    request();
+
+    
+  
+  });
 
 
-const getUserData = () => {
-    return fetch('https://jsonplaceholder.typicode.com/users/')
-    .then((res) => res.json())
-    .catch((err) => console.error(err));
-    //.then(json => console.log(json));
-};
-
-console.log(getUserData());
 
