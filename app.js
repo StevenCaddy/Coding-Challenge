2ce55107a34b934ab5f41805c2b7b1ed65d113ad
StevenCaddy <steven.caddie@gmail.com>
2022-04-14 20:17:13 -0500

    Basically Finished
    added clear function to empty the posts every time something is clicked
    
    only posts by that user are shown

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(() => {
     
     const request = () => {
         fetch(`https://jsonplaceholder.typicode.com/users/`)
@@ -6,18 +6,59 @@ $(document).ready(function(){
         .then((res) => {
             const user = document.getElementById('users')
             let item = ''
-            console.log(res)
+            //console.log(res)
             res.forEach(element => {
                 item = `${element.name} `
+                const li = document.createElement("li")
+                li.innerHTML = item
+                li.classList.add('item');
+                li.dataset.userId = element.id;
                 console.log(item)
-                user.append(item);
+                li.addEventListener('click', (event) => postRequest(event))
+                    user.appendChild(li);
             });
+
             
         })
         .catch((err) => console.error(err));
     }
+
+    const postRequest = (event) => {
+            const userId = event.target.dataset.userId;
     
+            fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+                .then((res) => res.json())
+                .then(json => renderPosts(json, event.target))
+        }
     
+    const renderPosts = (posts, target) => {
+        
+        clear();
+
+        const list = document.createElement("ul");
+
+        for (let i = 0; i < posts.length; i++) {
+
+            const item = document.createElement("li");
+            const liTitle = document.createElement("strong");
+            const liBody = document.createElement("p");
+
+            liTitle.innerHTML = posts[i].title;
+            liBody.innerHTML = posts[i].body;
+
+            item.appendChild(liTitle);
+            item.appendChild(liBody);
+            list.appendChild(item);
+        }
+
+        $('#posts').append(target.appendChild(list));
+    }
+
+    const clear = () => {
+        $('#posts').empty();
+        
+    }
+
     request();
 
     
